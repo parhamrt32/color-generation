3b8f4a3737bd2d0626ebfc0c3bba393dfdc85d85
parhamrt32 <92752702+parhamrt32@users.noreply.github.com>
2022-04-24 07:31:25 -0400

    final

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
-function SingleColor() {
-    return ( <> Single color  </> );
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
-export default SingleColor;
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+export default rgbToHex;
